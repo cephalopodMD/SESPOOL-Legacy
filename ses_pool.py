bf171a524fe705c620642b891035a5b876501ac9
mistahowe <mistahowe@gmail.com>
2013-05-01 15:31:25 -0300

    Update ses_pool.py

--- ses_pool.py
+++ ses_pool.py
@@ -20,7 +20,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 #
-#
 # Makes use of:
 # the python natural language toolkit library (nltk)
 # the brown corpus
